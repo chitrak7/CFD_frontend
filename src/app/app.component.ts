cd6da372f75138631f1efb9dcb36636702f7e9e0
chitrak7 <chitrak711988@gmail.com>
2018-04-01 08:53:50 +0530

    Added JSON support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,16 @@ import { AppService } from './app.service'
 @Component({
   selector: 'my-app',
   templateUrl:  './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent  { 
-   link: number = 0;
+  link: number = 1;
   load: number =  0;
-  res:string;
+  res: number = 0 
+  key_phrases:string;
+  bias_res:string;
+  fake_res: string;
+  sentiment: number;
 
    constructor(private appService: AppService) { }   
    setLink(a:number){
@@ -16,20 +21,43 @@ export class AppComponent  {
    }
 
    submitLink(link:string) {
+    this.load =  1;
+    console.log("submitted")
      this.appService.submitLink(link).subscribe(
-       res => {this.load =  1;
-        this.res = res;
+       res => {
+        console.log("sucess")
+        let resData = JSON.parse(JSON.stringify(res));
+        this.fake_res = resData.fake_result;
+        this.bias_res = this.bias_handle(resData.bias_result)
+        this.sentiment = resData.sentimentl;
+        this.key_phrases = resData.key_phrases;
         this.load = 0;
+        this.res=1
        }
      )
    }
 
    submitArticle(article:string){
+    this.load =  1;
      this.appService.submitArticle(article).subscribe(
-      res => {this.load =  1;
-       this.res = res;
+      res => {
+        let resData = JSON.parse(JSON.stringify(res));
+       this.fake_res = resData.fake_result;
+       this.bias_res = this.bias_handle(resData.bias_result);
+       this.sentiment = resData.sentimentl;
+       this.key_phrases = resData.key_phrases;
        this.load = 0;
+       this.res=1
       }
     )
+   }
+
+   bias_handle(a:string){
+     if (a=="1")
+        return "right"
+    else if (a=="-1")
+     return "left"
+     else 
+      return "neutral"
    }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -13,16 +13,14 @@ export class AppService {
     linkUrl = "http://localhost:8000/api/link"
     articleUrl = "http://localhost:8000/api/article"
     submitLink(a:string): Observable<string>{
-        
+	let url = this.linkUrl + "?url=" + a;      
         let params: HttpParams = new HttpParams();
         params.set('url', a)
-        return this.http.get<string>(this.linkUrl, {
-            params:params
-        })
+        return this.http.get<string>(url)
     }
 
     submitArticle(a:string): Observable <string>{
-        
+        let article = this.articleUrl + "?article=" + a; 
         let params: HttpParams = new HttpParams();
         params.set('article', a)
         return this.http.get<string>(this.articleUrl, {
